0363e2ae9d77febe0da33c6e053b8dd406f5a08a
markdanial <markdanial@gmail.com>
2024-11-10 20:42:15 -0800

    fixes auth

--- auth.config.ts
+++ auth.config.ts
@@ -12,9 +12,9 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } 
-      // else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
+      else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
       return true;
     },
   },
